8d1ef230b6af4e584f0814d3216316b5c8e1909d
maithili952006 <rautmaithili65@gmail.com>
2026-07-28 19:26:39 +0530

    Time: 6 ms (60.67%), Space: 43 MB (55.14%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,32 +1,60 @@
-class Solution {
-    public int countSubstrings(String s) {
+// class Solution {
+//     public int countSubstrings(String s) {
         
-        int count = 0;
-        for(int i = 0;i < s.length();i++){
-            for(int j = i; j < s.length();j++){
-                String sub = s.substring(i,j+1);
+//         int count = 0;
+//         for(int i = 0;i < s.length();i++){
+//             for(int j = i; j < s.length();j++){
+//                 String sub = s.substring(i,j+1);
 
-                if(isPalindrome(sub)){
-                    count++;
-                }
-            }
+//                 if(isPalindrome(sub)){
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+
+//     private boolean isPalindrome(String s){
+//             int left = 0;
+//             int right = s.length() - 1;
+
+//             while (left < right){
+//                     if(s.charAt(left) != s.charAt(right)){
+//                             return false;
+//                         }
+//                         left++;
+//                         right--;
+//                     }
+
+//                 return true;
+//     }        
+// }
+
+class Solution{
+    public int countSubstrings(String s){
+
+        int count = 0;
+
+        for(int i = 0; i < s.length();i++){
+            count += expandAroundCenter(s,i,i);
+
+            count += expandAroundCenter(s,i,i+1);
         }
 
         return count;
     }
 
-    private boolean isPalindrome(String s){
-            int left = 0;
-            int right = s.length() - 1;
+    private int expandAroundCenter(String s, int left,int right){
+        int count = 0;
 
-            while (left < right){
-                    if(s.charAt(left) != s.charAt(right)){
-                            return false;
-                        }
-                        left++;
-                        right--;
-                    }
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            count++;
 
-                return true;
-    }        
+            left--;
+            right++;
+        }
+
+        return count;
+    }
 }
